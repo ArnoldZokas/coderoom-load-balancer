3927a9d2bde6a1b800e3e0eb499dd899a82ff703
Arnold Zokas <arnold.zokas@coderoom.net>
2013-04-27 00:07:07 +0100

    Cleaned up DebugConsoleHost

---
+++ Coderoom.LoadBalancer/DebugConsoleHost.cs
@@ -0,0 +1,36 @@
+﻿#if DEBUG
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Coderoom.LoadBalancer
+{
+	internal class DebugConsoleHost
+	{
+		public static void Main()
+		{
+			var listeningTarge = new IPEndPoint(new IPAddress(new byte[] {127, 0, 0, 1}), 81);
+			var contentServers = new List<IPEndPoint>
+				{
+					new IPEndPoint(new IPAddress(new byte[] {127, 0, 0, 1}), 8081),
+					new IPEndPoint(new IPAddress(new byte[] {127, 0, 0, 1}), 8082)
+				};
+
+			Console.WriteLine("SIMPLE LOAD BALANCER");
+			Console.WriteLine("====================");
+			Console.WriteLine("Configuration:");
+			Console.WriteLine("    Target: {0}:{1}", listeningTarge.Address, listeningTarge.Port);
+			Console.WriteLine("    Content servers:");
+			foreach (IPEndPoint contentServer in contentServers)
+				Console.WriteLine("        * {0}:{1}", contentServer.Address, contentServer.Port);
+
+			var httpProxy = new HttpProxy(listeningTarge, contentServers);
+			httpProxy.Start();
+
+			Console.WriteLine();
+			Console.WriteLine("Load Balancer started...");
+			Console.WriteLine();
+		}
+	}
+}
+#endif
