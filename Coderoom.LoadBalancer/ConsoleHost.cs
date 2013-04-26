863b740a6184bece6a45b410b73a00058578edfd
Arnold Zokas <arnold.zokas@coderoom.net>
2013-04-26 23:54:37 +0100

    Cleaned up HttpProxy

---
+++ Coderoom.LoadBalancer/ConsoleHost.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Coderoom.LoadBalancer
+{
+	internal class ConsoleHost
+	{
+		public static void Main()
+		{
+			var servers = new List<IPEndPoint>
+				{
+					new IPEndPoint(new IPAddress(new byte[] {127, 0, 0, 1}), 8081),
+					new IPEndPoint(new IPAddress(new byte[] {127, 0, 0, 1}), 8082)
+				};
+
+			var httpProxy = new HttpProxy(new IPEndPoint(new IPAddress(new byte[] { 127, 0, 0, 1 }), 81), servers);
+			httpProxy.Start();
+		}
+	}
+}
---
+++ Coderoom.LoadBalancer/HttpProxy.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Coderoom.LoadBalancer
+{
+	public class HttpProxy
+	{
+		private readonly TcpListener _listener;
+		private readonly Thread _listenerThread;
+		private readonly IEnumerable<IPEndPoint> _servers;
+
+		public HttpProxy(IPEndPoint target, IEnumerable<IPEndPoint> servers)
+		{
+			_listener = new TcpListener(target);
+			_servers = servers;
+			_listenerThread = new Thread(Listen);
+		}
+
+		public void Start()
+		{
+			_listenerThread.Start(_listener);
+		}
+
+		private void Listen(object obj)
+		{
+			var listener = (TcpListener)obj;
+			listener.Start();
+
+			while (true)
+			{
+				var tcpClient = listener.AcceptTcpClient();
+				
+				ProcessRequest(tcpClient);
+				
+				tcpClient.Close();
+			}
+		}
+
+		private void ProcessRequest(TcpClient client)
+		{
+			var selectedServer = _servers.First();
+
+			using (var stream = client.GetStream())
+			{
+				using (var streamReader = new StreamReader(stream))
+				{
+					var header = streamReader.ReadLine();
+					var path = header.Split(' ')[1];
+
+					var requestUri = new Uri(string.Format("http://{0}:{1}{2}", selectedServer.Address, selectedServer.Port, path));
+					var proxiedRequest = WebRequest.Create(requestUri);
+
+					using (var proxiedResponse = proxiedRequest.GetResponse())
+					{
+						using (var proxiedResponseStream = proxiedResponse.GetResponseStream())
+						{
+							using (var proxiedResponseStreamReader = new StreamReader(proxiedResponseStream))
+							{
+								var proxiedResponseBody = proxiedResponseStreamReader.ReadToEnd();
+
+								var responseBuilder = new StringBuilder();
+								responseBuilder.AppendLine("HTTP/1.1 200 OK");
+								using (var swriter = new StreamWriter(stream))
+								{
+									foreach (var headerKey in proxiedResponse.Headers.AllKeys)
+									{
+										responseBuilder.AppendLine(string.Format("{0}: {1}", headerKey, proxiedResponse.Headers[headerKey]));
+									}
+
+									responseBuilder.AppendLine();
+									responseBuilder.Append(proxiedResponseBody);
+
+									var response = responseBuilder.ToString();
+									swriter.Write(response);
+									swriter.Flush();
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
