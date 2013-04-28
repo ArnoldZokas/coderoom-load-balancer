c15ebcd22b48cf1f339674ce1b2ee8f13904aaa5
Arnold Zokas <arnold.zokas@coderoom.net>
2013-04-28 14:06:04 +0100

    Applied GlobalAssemblyInfo

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("https://github.com/ArnoldZokas/coderoom-load-balancer")]
+[assembly: AssemblyCopyright("Copyright © 2013 Arnold Zokas")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
