4a333a02b29bbf14358833dcc7f11530c0211dd0
Arnold Zokas <arnold.zokas@coderoom.net>
2013-06-01 05:11:19 +0100

    Added temporary logging.

--- Coderoom.LoadBalancer/DebugConsoleHost.cs
+++ Coderoom.LoadBalancer/DebugConsoleHost.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Net;
 
 namespace Coderoom.LoadBalancer
@@ -11,36 +10,39 @@ namespace Coderoom.LoadBalancer
 	{
 		public static void Main()
 		{
-			var endPoint = new IPEndPoint(new IPAddress(new byte[] {127, 0, 0, 1}), 81);
-			var contentServers = new List<IPEndPoint>
+			try
+			{
+				var endPoint = new IPEndPoint(new IPAddress(new byte[] { 127, 0, 0, 1 }), 81);
+				var contentServers = new List<IPEndPoint>
 				{
 					new IPEndPoint(new IPAddress(new byte[] {127, 0, 0, 1}), 8081),
 					new IPEndPoint(new IPAddress(new byte[] {127, 0, 0, 1}), 8082)
 				};
 
-			Console.WriteLine("SIMPLE LOAD BALANCER");
-			Console.WriteLine("====================");
-			Console.WriteLine("Configuration:");
-			Console.WriteLine("    Target: {0}:{1}", endPoint.Address, endPoint.Port);
-			Console.WriteLine("    Content servers:");
-			foreach (var contentServer in contentServers)
-				Console.WriteLine("        * {0}:{1}", contentServer.Address, contentServer.Port);
+				Console.WriteLine("SIMPLE LOAD BALANCER");
+				Console.WriteLine("====================");
+				Console.WriteLine("Configuration:");
+				Console.WriteLine("    Target: {0}:{1}", endPoint.Address, endPoint.Port);
+				Console.WriteLine("    Content servers:");
+				foreach (var contentServer in contentServers)
+					Console.WriteLine("        * {0}:{1}", contentServer.Address, contentServer.Port);
 
-			try
-			{
 				var portListener = new PortListener(endPoint);
 				var httpProxy = new HttpProxy(contentServers, portListener);
 				httpProxy.Start();
 				portListener.ConnectionEstablished += (sender, args) => Console.WriteLine("Connection established");
+
+				Console.WriteLine();
+				Console.WriteLine("Load Balancer started...");
+				Console.WriteLine();
+
 			}
 			catch (Exception ex)
 			{
-				File.WriteAllText("C:\\Deployment\\log.txt", ex.Message + " || " + ex.StackTrace);
+				Console.WriteLine(ex.Message);
+				Console.WriteLine();
+				Console.WriteLine(ex.StackTrace);
 			}
-
-			Console.WriteLine();
-			Console.WriteLine("Load Balancer started...");
-			Console.WriteLine();
 		}
 	}
 }
